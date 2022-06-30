ce8b83e8ae2c7479e793fa73fc3a3ceede302dab
jeomxon <s_n_i_p_e_r_@naver.com>
2022-06-30 22:06:26 +0900

    init

---
+++ src/test/java/com/mogaco/what_was_it_backend/WhatWasItBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mogaco.what_was_it_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatWasItBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
